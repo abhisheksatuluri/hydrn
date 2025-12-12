e98f5dd1601400c6d2ffdf1dbeeb492c9064514a
abhishek <abhisheksatuluri@gmail.com>
2025-12-12 14:54:10 +0530

    fix: Move assets to public folder for production availability

---
+++ rewrite_auth.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export GIT_AUTHOR_NAME="abhishek"
+export GIT_AUTHOR_EMAIL="abhisheksatuluri@gmail.com"
+export GIT_COMMITTER_NAME="abhishek"
+export GIT_COMMITTER_EMAIL="abhisheksatuluri@gmail.com"
